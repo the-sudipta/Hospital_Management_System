de3fa5b4962069bcac773ef50eae0b722ae29ab1
Sudipta Kumar <160587586+the-sudipta@users.noreply.github.com>
2025-02-20 17:28:52 +0600

    Update Admin_Create_UserController.php

--- controller/admin/Admin_Create_UserController.php
+++ controller/admin/Admin_Create_UserController.php
@@ -5,8 +5,9 @@ global $routes;
 require '../../routes.php';
 
 
-require_once __DIR__ . '/../../model/CalculationRepo.php';
-require_once __DIR__ . '/../../model/userRepo.php';
+require_once dirname(__DIR__) . '/model/CalculationRepo.php';
+require_once dirname(__DIR__) . '/model/userRepo.php';
+
 
 
 @session_start();
