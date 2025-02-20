a9edc5820c378afbfc59916a3f6cfd5e1ecc5c3f
Sudipta Kumar <160587586+the-sudipta@users.noreply.github.com>
2025-02-20 19:35:13 +0600

    Update CreateBillController.php

--- controller/his/CreateBillController.php
+++ controller/his/CreateBillController.php
@@ -5,7 +5,7 @@ global $routes;
 require '../../routes.php';
 
 
-require_once __DIR__ . '/../../model/billingRepo.php';
+require_once dirname(__DIR__) . '/../model/billingRepo.php';
 
 
 $Login_page = $routes['login'];
